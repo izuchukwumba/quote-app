5637f580ec39dbf1ba59c465e698b6380216abec
Izuchukwu Mba <45977460+izuchukwumba@users.noreply.github.com>
2023-07-12 15:19:38 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+import {allQuotes} from './quotes.js';
+
+let author = document.querySelector(".person");
+let quote = document.querySelector(".quote");
+let button = document.querySelector(".button");
+
+
+
+function randomQuote(){
+let random = Math.floor(Math.random() * allQuotes.length);
+quote.innerText = '"' + allQuotes[random].quote + '"';
+author.innerText = allQuotes[random].author;
+}
+
+button.addEventListener("click", randomQuote);
+
+
+
+
+console.log(allQuotes.length);
