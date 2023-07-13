3e937df243c469643a0d1a7e5083aa91435e19ad
Izuchukwu Mba <45977460+izuchukwumba@users.noreply.github.com>
2023-07-13 00:57:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 //Importing quote data from quotes.js
 import {allQuotes} from './quotes.js';
 
+// Selecting just the needed data from the array (Quote and Author's name)
+// const onlyQuoteAndAuthorName = allQuotes.map(obj => {
+//     const newObj = {};
+//     const keys = Object.keys(obj);
+//     newObj[keys[0]] = obj[keys[0]];
+//     newObj[keys[1]] = obj[keys[1]];
+//     return newObj;
+//   });
+
+//Selecting only quotes that are less or equal to 90 characters
+let shortQuotes = onlyQuoteAndAuthorName.filter(obj => {
+    let keys = Object.keys(obj);
+    let value = obj[keys[0]];
+    return value.length <= 90;
+    });
+
+// DOM Manipulation
 let author = document.querySelector(".person");
 let quote = document.querySelector(".quote");
 let button = document.querySelector(".button");
