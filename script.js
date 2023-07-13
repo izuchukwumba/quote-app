171ca2c784e72fe5590c0674cf0a351df42fefd5
Izuchukwu Mba <45977460+izuchukwumba@users.noreply.github.com>
2023-07-12 22:48:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,4 +12,5 @@ quote.innerText = '"' + allQuotes[random].quote + '"';
 author.innerText = allQuotes[random].author;
 }
 
+document.addEventListener("DOMContentLoaded", randomQuote);
 button.addEventListener("click", randomQuote);
