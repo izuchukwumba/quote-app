f6869a1cfbf0a3be94ec81ea2e518ab5716c9af6
Izuchukwu Mba <45977460+izuchukwumba@users.noreply.github.com>
2023-07-14 11:52:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
 //Importing quote data from quotes.js
 import {allQuotes} from './quotes.js';
 
-// Selecting just the needed data from the array (Quote and Author's name)
-// const onlyQuoteAndAuthorName = allQuotes.map(obj => {
-//     const newObj = {};
-//     const keys = Object.keys(obj);
-//     newObj[keys[0]] = obj[keys[0]];
-//     newObj[keys[1]] = obj[keys[1]];
-//     return newObj;
-//   });
-
-//Selecting only quotes that are less or equal to 90 characters
+//Selecting only quotes that are less or equal to 90 characters, and without Kanye obscenities
 let shortQuotes = allQuotes.filter(obj => {
     let keys = Object.keys(obj);
     let quoteValue = obj[keys[0]];
     let authorValue = obj[keys[1]];
     return quoteValue.length <= 90 && authorValue.length <= 24 && !authorValue.toLowerCase().includes("proverb") && !authorValue.toLowerCase().includes("kanye");
 });
-console.log(shortQuotes.length)
 
 // DOM Manipulation
 let author = document.querySelector(".person");
 let quote = document.querySelector(".quote");
-let button = document.querySelector(".button");
 let signature = document.querySelector(".signature");
-
+let button = document.querySelector(".btn-newquote");
+let tweetButton = document.querySelector(".btn-tweet");
+let copyButton = document.querySelector(".btn-copy");
+let copyText = document.querySelector("#copyText")
 // Generate a random quote and assign it, the author's name and signature to their respective html elements
 function randomQuote(){
-    let random = Math.floor(Math.random() * shortQuotes.length);
+    
+  let random = Math.floor(Math.random() * shortQuotes.length);
     quote.innerText = '"' + shortQuotes[random].quote + '"';
     author.innerText = shortQuotes[random].author;
 
@@ -43,8 +36,32 @@ function randomQuote(){
     else{
         let middleInitial = fullName[1][0].toUpperCase();
         signature.innerText = firstInitial + "." + middleInitial + "." + lastInitial;
+    };
+
+    window.quoteAndAuthorTweet = quote.innerText + "\n" + "\n" + "-" + " " + author.innerText;
+
+    if (copyText.innerText === 'Copied!'){
+      copyText.innerText = 'Copy Quote'
     }
 };
 
+
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteAndAuthorTweet}`;
+  window.open(twitterUrl, "_blank");
+  console.log(twitterUrl)
+}
+
+function copyQuote(){
+  navigator.clipboard.writeText(quoteAndAuthorTweet);
+  copyText.innerHTML = '<span class = "btn-text" id = "copyText">Copied!</span>'
+}
+
 document.addEventListener("DOMContentLoaded", randomQuote);
 button.addEventListener("click", randomQuote);
+tweetButton.addEventListener("click", tweetQuote);
+copyButton.addEventListener("click", copyQuote);
+
+
+
+console.log(window.getComputedStyle(copyButton).getPropertyValue('width'));
